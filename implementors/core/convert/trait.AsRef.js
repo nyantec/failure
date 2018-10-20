e35015c6663f5d1ef4865b1b8659cb9917f78299
Armin Ronacher <armin.ronacher@active-4.com>
2018-10-20 23:23:00 +0200

    docs

---
+++ implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["failure"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a>&gt; for <a class=\"struct\" href=\"failure/struct.Error.html\" title=\"struct failure::Error\">Error</a>",synthetic:false,types:["failure::error::Error"]},];
+implementors["quote"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"quote/struct.Tokens.html\" title=\"struct quote::Tokens\">Tokens</a>",synthetic:false,types:["quote::tokens::Tokens"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"quote/struct.Ident.html\" title=\"struct quote::Ident\">Ident</a>",synthetic:false,types:["quote::ident::Ident"]},];
+implementors["syn"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"syn/struct.Ident.html\" title=\"struct syn::Ident\">Ident</a>",synthetic:false,types:["syn::ident::Ident"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
